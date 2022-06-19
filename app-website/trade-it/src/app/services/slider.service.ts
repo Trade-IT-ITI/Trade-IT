1a8b0117f5e3f4d0207ef0653a691eceb8b47cbc
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-06-19 02:21:47 +0200

    creating slider

--- app-website/trade-it/src/app/services/slider.service.ts
+++ app-website/trade-it/src/app/services/slider.service.ts
@@ -5,9 +5,9 @@ import { SliderImage } from '../models/sliderImage';
   providedIn: 'root'
 })
 export class SliderService {
-  img1: SliderImage = { sliderImageId: 1, name: 'beach.png', sliderText: 'march sales are now available' };
-  img2: SliderImage = { sliderImageId: 2, name: 'mountain.jpg', sliderText: 'our belives' };
-  img3: SliderImage = { sliderImageId: 3, name: 'paris.jpg', sliderText: '50% discount on all products' };
+  img1: SliderImage = { sliderImageId: 1, name: 'first.jfif', sliderText: 'march sales are now available' };
+  img2: SliderImage = { sliderImageId: 2, name: 'second.jfif', sliderText: 'our belives' };
+  img3: SliderImage = { sliderImageId: 3, name: 'third.jfif', sliderText: '50% discount on all products' };
 
   images: SliderImage[] = [this.img1, this.img2, this.img3]
 
