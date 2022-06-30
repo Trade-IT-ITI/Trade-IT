36038528ac2b9b0680a3e21cb4566c5450ee3f20
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-06-30 10:20:03 +0200

    edit expand function to use expression tree

--- app-api/API/API Layer/Helper/IQueryableHelper.cs
+++ app-api/API/API Layer/Helper/IQueryableHelper.cs
@@ -7,34 +7,51 @@ namespace API_Layer.Helper
     {
         public static void Expand<T>(this IQueryable<T> Set , string[] properties) where T : class
         {
-            Type type = typeof(EntityFrameworkQueryableExtensions);
-            var includeMethod = type.GetMethods().SingleOrDefault(mi => mi.Name == "Include" && mi.ReturnType.Name == "IQueryable`1");
-            var genericIncludeMethod = includeMethod?.MakeGenericMethod(typeof(T));
+            //using reflection
 
-            foreach (string prop in properties)
+            //Type type = typeof(EntityFrameworkQueryableExtensions);
+            //var includeMethod = type.GetMethods().SingleOrDefault(mi => mi.Name == "Include" && mi.ReturnType.Name == "IQueryable`1");
+            //var genericIncludeMethod = includeMethod?.MakeGenericMethod(typeof(T));
+
+            //foreach (string prop in properties)
+            //{
+            //    if (typeof(T).GetProperty(prop) != null)
+            //        Set = (IQueryable<T>) genericIncludeMethod.Invoke(null , new object[] { Set , prop });
+            //}
+            //Set.Load();
+
+            //--------------------------------
+
+            //using expression tree
+
+            var parameter = Expression.Parameter(typeof(T));
+            foreach (string propertyName in properties)
             {
-                if (typeof(T).GetProperty(prop) != null)
-                    Set = (IQueryable<T>) genericIncludeMethod.Invoke(null , new object[] { Set , prop });
+                if (typeof(T).GetProperty(propertyName) != null)
+                {
+                    var property = Expression.Property(parameter , propertyName);
+                    var exp = Expression.Lambda<Func<T , object>>(property , parameter);
+                    Set = Set.Include(exp);
+                }
             }
             Set.Load();
+
         }
         public static IQueryable<T> Sort<T>(this IQueryable<T> source , string propertyName , string sortType)
         {
             if (Enum.TryParse(sortType , out SortType type))
             {
+                var parameter = Expression.Parameter(typeof(T));
+                var property = Expression.Property(parameter , propertyName);
+                var propAsObject = Expression.Convert(property , typeof(object));
+
+                var exp = Expression.Lambda<Func<T , object>>(propAsObject , parameter);
+
                 return (type == SortType.asc)
-                    ? source.OrderBy(ToLambda<T>(propertyName))
-                    : source.OrderByDescending(ToLambda<T>(propertyName));
+                    ? source.OrderBy(exp)
+                    : source.OrderByDescending(exp);
             }
             return source;
-        }
-        private static Expression<Func<T , object>> ToLambda<T>(string propertyName)
-        {
-            var parameter = Expression.Parameter(typeof(T));
-            var property = Expression.Property(parameter , propertyName);
-            var propAsObject = Expression.Convert(property , typeof(object));
-
-            return Expression.Lambda<Func<T , object>>(propAsObject , parameter);
         }
     }
 }
