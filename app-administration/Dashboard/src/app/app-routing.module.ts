f97e8c468e15bc73ec46a7ffee2332c6698b130b
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-07-08 17:44:00 +0200

    side bar

--- app-administration/Dashboard/src/app/app-routing.module.ts
+++ app-administration/Dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './Category/add-category/add-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"category", component: AddCategoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-administration/Dashboard/src/app/app.module.ts
+++ app-administration/Dashboard/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCategoryComponent } from './Category/add-category/add-category.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddCategoryComponent
+    AddCategoryComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
