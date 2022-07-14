ad6d2367c253fefbb4d151985119bf518285df4b
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-07-14 20:33:53 +0200

    Add Area

--- app-administration/Dashboard/src/app/app-routing.module.ts
+++ app-administration/Dashboard/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddCityComponent } from './component/Location/City/add-city/add-city.component';
-import { AddCategoryComponent } from './component/Category/add-category/add-category.component';
-import { AddSubcategoryComponent } from './component/Subcategory/add-subcategory/add-subcategory.component';
+
+import { AddCityComponent } from 'src/app/component/Location/City/add-city/add-city.component';
+import { AddCategoryComponent } from 'src/app/component/Category/add-category/add-category.component';
+import { AddSubcategoryComponent } from 'src/app/component/Subcategory/add-subcategory/add-subcategory.component';
+import { AddAreaComponent } from 'src/app/component/Location/Area/add-area/add-area.component';
 
 const routes: Routes = [
   {path:"category", component: AddCategoryComponent},
   {path:"subcategory", component: AddSubcategoryComponent},
-  {path:"city", component: AddCityComponent}
+  {path:"city", component: AddCityComponent},
+  {path:"area", component: AddAreaComponent}
 ];
 
 @NgModule({
--- app-administration/Dashboard/src/app/services/area.service.ts
+++ app-administration/Dashboard/src/app/services/area.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class AreaService {
+export class AreaService  {
+  public url = "http://localhost:5228/api/Area";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addArea(name: string,cityId: number) {
+    return this.http.post<any>(this.url, { name,cityId })
+      .pipe(catchError(this.handleError));
+    
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `, JSON.stringify(error.error));
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 }
