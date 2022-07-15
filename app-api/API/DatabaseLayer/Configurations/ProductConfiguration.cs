0ff72c5618018a9d2fec56775be3a949b24e8078
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-07-15 09:39:55 +0200

    fix images bug

--- app-api/API/DatabaseLayer/Configurations/ProductConfiguration.cs
+++ app-api/API/DatabaseLayer/Configurations/ProductConfiguration.cs
@@ -14,14 +14,98 @@ namespace DatabaseLayer
             builder.Property(p => p.ViewsCount).HasDefaultValue(0);
             builder.Property(p => p.RequestCount).HasDefaultValue(0);
             builder.HasData(
-                new Product() { ProductId = 1 , Title = "Lenovo Ideapad 330" , Descrioption = "Lenovo Ideapad 330 Laptop, Intel Core i3-7020U, 15.6 Inch, 1TB, 4GB RAM, DOS - Platinum Grey" , PostDateTime = new DateTime(2022 , 06 , 15) , RequestCount = 2 , ViewsCount = 20 , StatusId = 1 , SubcategoryId = 1 , CityId = 1 , AreaId = 2 , Price = 8000 , UserId = 2 } ,
-                new Product() { ProductId = 2 , Title = "Redmi note 9" , Descrioption = "Redmi Note 9 is equipped with a high-performance octa-core processor with a maximum clock speed of 3.0GHz, a GPU frequency of 1000MHz, for improved performance to provide you with a seamless gaming experience and implement your ideas." , PostDateTime = new DateTime(2022 , 06 , 28) , RequestCount = 10 , ViewsCount = 88 , StatusId = 2 , SubcategoryId = 2 , CityId = 2 , AreaId = 3 , Price = 3450 , UserId = 1 },
-                new Product() { ProductId = 3 , Title = "Gaming Chair ITop 220" , Descrioption = "new gaming chair 2022 model" , PostDateTime = new DateTime(2022 , 05 , 12) , RequestCount = 5 , ViewsCount = 60 , StatusId = 1 , SubcategoryId = 3 , CityId = 3 , AreaId = 5 , Price = 6500 , UserId = 2 },
-                new Product() { ProductId = 4 , Title = "children shirt" , Descrioption = "children shirt small and medium sizes" , PostDateTime = new DateTime(2022 , 06 , 28) , RequestCount = 3 , ViewsCount = 110 , StatusId = 1 , SubcategoryId = 4 , CityId = 3 , AreaId = 6 , Price = 50 , UserId = 2 },
-                new Product() { ProductId = 5 , Title = "boys t-shirt" , Descrioption = "boys t-shirt medium and large sizes" , PostDateTime = new DateTime(2022 , 01 , 13) , RequestCount = 1 , ViewsCount = 90 , StatusId = 2 , SubcategoryId = 7 , CityId = 1 , AreaId = 1 , Price = 100 , UserId = 1 },
-                new Product() { ProductId = 6 , Title = "girls dress" , Descrioption = "girls dress medium and large sizes" , PostDateTime = new DateTime(2021 , 12 , 05) , RequestCount = 0 , ViewsCount = 5 , StatusId = 2 , SubcategoryId = 8 , CityId = 2 , AreaId = 4 , Price = 250 , UserId = 1 }
-
-                );
+                new Product()
+                {
+                    ProductId = 1 ,
+                    Title = "Lenovo Ideapad 330" ,
+                    Descrioption = "Lenovo Ideapad 330 Laptop, Intel Core i3-7020U, 15.6 Inch, 1TB, 4GB RAM, DOS - Platinum Grey" ,
+                    PostDateTime = new DateTime(2022 , 06 , 15) ,
+                    RequestCount = 2 ,
+                    ViewsCount = 20 ,
+                    StatusId = 1 ,
+                    SubcategoryId = 1 ,
+                    CityId = 1 ,
+                    AreaId = 2 ,
+                    Price = 8000 ,
+                    UserId = 2 ,
+                    ProductImages = new List<ProductImage>() { }
+                } ,
+                new Product()
+                {
+                    ProductId = 2 ,
+                    Title = "Redmi note 9" ,
+                    Descrioption = "Redmi Note 9 is equipped with a high-performance octa-core processor with a maximum clock speed of 3.0GHz, a GPU frequency of 1000MHz, for improved performance to provide you with a seamless gaming experience and implement your ideas." ,
+                    PostDateTime = new DateTime(2022 , 06 , 28) ,
+                    RequestCount = 10 ,
+                    ViewsCount = 88 ,
+                    StatusId = 2 ,
+                    SubcategoryId = 2 ,
+                    CityId = 2 ,
+                    AreaId = 3 ,
+                    Price = 3450 ,
+                    UserId = 1
+                } ,
+                new Product()
+                {
+                    ProductId = 3 ,
+                    Title = "Gaming Chair ITop 220" ,
+                    Descrioption = "new gaming chair 2022 model" ,
+                    PostDateTime = new DateTime(2022 , 05 , 12) ,
+                    RequestCount = 5 ,
+                    ViewsCount = 60 ,
+                    StatusId = 1 ,
+                    SubcategoryId = 3 ,
+                    CityId = 3 ,
+                    AreaId = 5 ,
+                    Price = 6500 ,
+                    UserId = 2
+                } ,
+                new Product()
+                {
+                    ProductId = 4 ,
+                    Title = "children shirt" ,
+                    Descrioption = "children shirt small and medium sizes" ,
+                    PostDateTime = new DateTime(2022 , 06 , 28) ,
+                    RequestCount = 3 ,
+                    ViewsCount = 110 ,
+                    StatusId = 1 ,
+                    SubcategoryId = 4 ,
+                    CityId = 3 ,
+                    AreaId = 6 ,
+                    Price = 50 ,
+                    UserId = 2 ,
+                } ,
+                new Product()
+                {
+                    ProductId = 5 ,
+                    Title = "boys t-shirt" ,
+                    Descrioption = "boys t-shirt medium and large sizes" ,
+                    PostDateTime = new DateTime(2022 , 01 , 13) ,
+                    RequestCount = 1 ,
+                    ViewsCount = 90 ,
+                    StatusId = 2 ,
+                    SubcategoryId = 7 ,
+                    CityId = 1 ,
+                    AreaId = 1 ,
+                    Price = 100 ,
+                    UserId = 1
+                } ,
+                new Product()
+                {
+                    ProductId = 6 ,
+                    Title = "girls dress" ,
+                    Descrioption = "girls dress medium and large sizes" ,
+                    PostDateTime = new DateTime(2021 , 12 , 05) ,
+                    RequestCount = 0 ,
+                    ViewsCount = 5 ,
+                    StatusId = 2 ,
+                    SubcategoryId = 8 ,
+                    CityId = 2 ,
+                    AreaId = 4 ,
+                    Price = 250 ,
+                    UserId = 1
+                }
+            );
         }
     }
 }
--- app-api/API/DatabaseLayer/Configurations/ProductImageConfiguration.cs
+++ app-api/API/DatabaseLayer/Configurations/ProductImageConfiguration.cs
@@ -9,8 +9,13 @@ namespace DatabaseLayer
         public void Configure(EntityTypeBuilder<ProductImage> builder)
         {
             builder.HasData(
-                new ProductImage() { ProductImageId = 1 , Name = "lenovo_x1.jpg" , ProductId = 1 } ,
-                new ProductImage() { ProductImageId = 2 , Name = "lenovo_x1_2.jpg" , ProductId = 1 } 
+                new ProductImage() { ProductImageId = 1 , ProductId = 1 , Name = "lenovo1.jpg" } ,
+                new ProductImage() { ProductImageId = 2 , ProductId = 1 , Name = "lenovo2.jpg" } ,
+                new ProductImage() { ProductImageId = 3 , ProductId = 2 , Name = "152419-review-hands-on-redmi-note-9-image1-rr6mm2jbpp.jpg" } ,
+                new ProductImage() { ProductImageId = 4 , ProductId = 3 , Name = "nacon-gaming-chair-blue-pcch-310-bl.jpg" } ,
+                new ProductImage() { ProductImageId = 5 , ProductId = 4 , Name = "New-Summer-Children-shirts-Printing-Anchor-pattern-Cotton-100-Short-sleeved-Boy-s-shirts-Fit-for.jpg" } ,
+                new ProductImage() { ProductImageId = 6 , ProductId = 5 , Name = "16828269_35035155_300.jpg" } ,
+                new ProductImage() { ProductImageId = 7 , ProductId = 6 , Name = "c827e760-197a-4dde-83fe-2975d46017de_large.jpg" }
                 );
         }
     }
