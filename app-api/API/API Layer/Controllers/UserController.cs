6a29d83edb8f709c518001a74fbcc124a8c36321
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-07-15 18:01:08 +0200

    add login and register actions

---
+++ app-api/API/API Layer/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using API_Layer.Repositories.Interfaces;
+using DatabaseLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Layer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(string email , string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+                return BadRequest("email and password are required");
+
+            var user = await _userRepository.GetByEmail(email);
+            if (user == null)
+                return NotFound("no user exists with this email");
+
+            if (user.Password != password.ToLower())
+                return NotFound("wrong password");
+
+            return Ok(user);
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            try
+            {
+                await _userRepository.Add(user);
+                return Ok(user);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ app-api/API/DatabaseLayer/Migrations/20220715154908_edit-user.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseLayer.Migrations
+{
+    public partial class edituser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "Email", "Password" },
+                values: new object[] { "abdoahmed@yahoo.com", "890" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                columns: new[] { "Email", "Password" },
+                values: new object[] { "marwansayed@gmail.com", "321" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+    }
+}
