7e27da00da875185b9e9d23b0dddfbe20a18ce5a
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-07-18 18:53:09 +0200

    correct naming

--- app-website/trade-it/src/app/app.module.ts
+++ app-website/trade-it/src/app/app.module.ts
@@ -3,24 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './app-layout/navbar/navbar.component';
-import { NavbarCategoriesComponent } from './app-layout/navbar-categories/navbar-categories.component';
-import { HomeComponent } from './app-pages/home/home.component';
-import { SliderComponent } from './app-pages/slider/slider.component';
-import { ProductsComponent } from './app-pages/products/products.component';
-import { DurationPipe } from './pipes/duration.pipe';
-import { FooterComponent } from './app-layout/footer/footer.component';
-import { PagesNavComponent } from './app-layout/pages-nav/pages-nav.component';
-import { SearchComponent } from './app-pages/search/search.component';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
-import { AppRoutingModule } from './app.routing';
-import { NotFoundComponent } from './app-pages/not-found/not-found.component';
+
+import { AppComponent } from 'src/app/app.component';
+import { NavbarComponent } from 'src/app/app-layout/navbar/navbar.component';
+import { NavbarCategoriesComponent } from './app-layout/navbar-categories/navbar-categories.component';
+import { HomeComponent } from 'src/app/app-pages/home/home.component';
+import { SliderComponent } from 'src/app/app-pages/slider/slider.component';
+import { ProductsComponent } from 'src/app/app-pages/product-pages/products/products.component';
+import { DurationPipe } from 'src/app/pipes/duration.pipe';
+import { FooterComponent } from 'src/app/app-layout/footer/footer.component';
+import { PagesNavComponent } from 'src/app/app-layout/pages-nav/pages-nav.component';
+import { SearchComponent } from 'src/app/app-pages/search/search.component';
+import { AppRoutingModule } from 'src/app/app.routing';
+import { NotFoundComponent } from 'src/app/app-pages/not-found/not-found.component';
 import { AdminModule } from './admin/admin.module';
-import { ProdutDetailsComponent } from './app-pages/produt-details/produt-details.component';
-import { LoginComponent } from './app-pages/auth/login/login.component';
-import { RegisterComponent } from './app-pages/auth/register/register.component';
+import { ProductDetailsComponent } from 'src/app/app-pages/product-pages/product-details/product-details.component';
+import { LoginComponent } from 'src/app/app-pages/auth/login/login.component';
+import { RegisterComponent } from 'src/app/app-pages/auth/register/register.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +35,7 @@ import { RegisterComponent } from './app-pages/auth/register/register.component'
     PagesNavComponent,
     SearchComponent,
     NotFoundComponent,
-    ProdutDetailsComponent,
+    ProductDetailsComponent,
     LoginComponent,
     RegisterComponent,
   ],
