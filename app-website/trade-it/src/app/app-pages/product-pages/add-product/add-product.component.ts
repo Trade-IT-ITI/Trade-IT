67188dbc11517b95f1427cf90d371f0ecc6568e1
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-07-19 05:20:09 +0200

    add product need edit

--- app-website/trade-it/src/app/app-pages/product-pages/add-product/add-product.component.ts
+++ app-website/trade-it/src/app/app-pages/product-pages/add-product/add-product.component.ts
@@ -1,9 +1,14 @@
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 import { Area } from 'src/app/models/area';
 import { category } from 'src/app/models/category';
 import { City } from 'src/app/models/city';
 import { subcategory } from 'src/app/models/subcategory';
+import { CategoryService } from 'src/app/services/category.service';
+import { CityService } from 'src/app/services/city.service';
 
 @Component({
   selector: 'app-add-product',
@@ -11,94 +16,94 @@ import { subcategory } from 'src/app/models/subcategory';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  title: string = "";
+  descrioption: string = "";
+  price: number = 1;
   cityId: number = 0;
   areaId: number = 0;
   categoryId: number = 0;
   subcategoryId: number = 0;
-
+  userId: number = 0;
   //location
   cities: City[] = []
   areas: Area[] = []
   isAreasDisabled: boolean = true;
 
-   //categories
-   categories: category[] = []
-   subcategories: subcategory[] = []
-   isSubcategoriesDisabled: boolean = true;
-  constructor() { }
+  //categories
+  categories: category[] = []
+  subcategories: subcategory[] = []
+  isSubcategoriesDisabled: boolean = true;
+  constructor(private cityService: CityService, private categoryService: CategoryService, private productService: ProductService) { }
 
   ngOnInit(): void {
+    let userData = localStorage.getItem('userData');
+    let user = userData != null ? JSON.parse(userData) : null
+    this.userId = user != null ? user.userId : 0;
+    let CitiesParams: HttpParams = new HttpParams();
+    CitiesParams = CitiesParams.append('expand', 'Areas');
+    this.cityService.getAll(CitiesParams).subscribe(data => {
+      this.cities = data;
+    })
+    let CategoriesParams: HttpParams = new HttpParams();
+    CategoriesParams = CategoriesParams.append('expand', 'Subcategories')
+    this.categoryService.getall(CategoriesParams).subscribe(data => {
+      this.categories = data;
+    })
   }
   //location
   onSelectCity() {
-    // if (this.cityId == 0) {
-    //   this.isAreasDisabled = true;
-    //   this.areas = [];
-    //   this.productsQueryParams = this.productsQueryParams.delete('city');
-    //   this.productsQueryParams = this.productsQueryParams.delete('area');
-    //   this.isFiltersClean = true;
-    // }
-    // else {
-    //   let city = this.cities.find(c => c.cityId == this.cityId);
-    //   if (city) {
-    //     this.isAreasDisabled = false
-    //     this.areas = city.areas;
-    //     if (this.productsQueryParams.has('city'))
-    //       this.productsQueryParams = this.productsQueryParams.set('city', this.cityId);
-    //     else
-    //       this.productsQueryParams = this.productsQueryParams.append('city', this.cityId);
-
-    //     this.productsQueryParams = this.productsQueryParams.delete('area');
-    //     this.isFiltersClean = false;
-    //   }
-    // }
-  }
-  onSelectArea() {
-    // if (this.areaId == 0) {
-    //   this.productsQueryParams = this.productsQueryParams.delete('area');
-    // }
-    // else {
-    //   if (this.productsQueryParams.has('area'))
-    //     this.productsQueryParams = this.productsQueryParams.set('area', this.areaId);
-    //   else
-    //     this.productsQueryParams = this.productsQueryParams.append('area', this.areaId);
-    // }
+    if (this.cityId == 0) {
+      this.isAreasDisabled = true;
+      this.areas = [];
+    }
+    else {
+      let city = this.cities.find(c => c.cityId == this.cityId);
+      if (city) {
+        this.isAreasDisabled = false
+        this.areas = city.areas;
+      }
+    }
   }
   //category
   onSelectCategory() {
-    // if (this.categoryId == 0) {
-    //   this.isSubcategoriesDisabled = true;
-    //   this.subcategories = [];
-    //   this.productsQueryParams = this.productsQueryParams.delete('category');
-    //   this.productsQueryParams = this.productsQueryParams.delete('subcategory');
-    //   this.isFiltersClean = true;
-
-    // }
-    // else {
-    //   let category = this.categories.find(c => c.categoryId == this.categoryId);
-    //   if (category) {
-    //     this.isSubcategoriesDisabled = false
-    //     this.subcategories = category.subcategories;
-    //     if (this.productsQueryParams.has('category'))
-    //       this.productsQueryParams = this.productsQueryParams.set('category', this.categoryId);
-    //     else
-    //       this.productsQueryParams = this.productsQueryParams.append('category', this.categoryId);
-
-    //     this.productsQueryParams = this.productsQueryParams.delete('subcategory');
-    //     this.isFiltersClean = false;
-
-    //   }
-    // }
+    if (this.categoryId == 0) {
+      this.isSubcategoriesDisabled = true;
+      this.subcategories = [];
+    }
+    else {
+      let category = this.categories.find(c => c.categoryId == this.categoryId);
+      if (category) {
+        this.isSubcategoriesDisabled = false
+        this.subcategories = category.subcategories;
+      }
+    }
   }
-  onSelectSubcategory() {
-    // if (this.subcategoryId == 0) {
-    //   this.productsQueryParams = this.productsQueryParams.delete('subcategory');
-    // } else {
-    //   if (this.productsQueryParams.has('subcategory'))
-    //     this.productsQueryParams = this.productsQueryParams.set('subcategory', this.subcategoryId);
-    //   else
-    //     this.productsQueryParams = this.productsQueryParams.append('subcategory', this.subcategoryId);
-    // }
 
+  //product data
+
+  file: File | null = null;
+  fileName = '';
+  onFileSelected(event: any) {
+    this.file = <File>event.target.files[0];
+    if (this.file) {
+      this.fileName = this.file.name;
+
+    }
   }
+  addProduct() {
+    const formData = new FormData();
+    formData.append('title', this.title);
+    formData.append('descrioption', this.descrioption);
+    formData.append('price', this.price.toString());
+    formData.append('cityId', this.cityId.toString());
+    formData.append('areaId', this.areaId.toString());
+    formData.append('subcategoryId', this.subcategoryId.toString());
+    formData.append('userId', this.userId.toString());
+    formData.append("image", this.file!);
+    console.log(formData)
+    this.productService.addProduct(formData).subscribe((product) => {
+      console.log(product);
+    });
+  }
+
 }
--- app-website/trade-it/src/app/services/product.service.ts
+++ app-website/trade-it/src/app/services/product.service.ts
@@ -1,22 +1,38 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Product } from '../models/product';
-import { ProductData } from '../models/productData';
-import { ProductsData } from '../models/productsData';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { ProductData } from 'src/app/models/productData';
+import { ProductsData } from 'src/app/models/productsData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  public url = "http://localhost:5228/api/Product"
   constructor(private http: HttpClient) {
   }
   getall(params?: HttpParams): Observable<ProductsData> {
-    return this.http.get<ProductsData>('http://localhost:5228/api/Product', {
-      params: params
-    })
+    return this.http.get<ProductsData>(this.url, { params: params })
+      .pipe(catchError(this.handleError));
   };
   getById(id: number): Observable<ProductData> {
-    return this.http.get<ProductData>(`http://localhost:5228/api/Product/${id}`);
+    return this.http.get<ProductData>(`${this.url}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+  addProduct(product: any) {
+    return this.http.post<any>(this.url,{product})
+      .pipe(catchError(this.handleError));
+    ;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `, JSON.stringify(error.error));
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
